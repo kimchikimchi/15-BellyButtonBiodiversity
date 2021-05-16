f9ef76920a703fa73fe5da92e4ee12bba8593cc1
Jay Cha <kimchikimchi@gmail.com>
2021-05-15 23:08:59 -0400

    - Replaced splice function with slice as I want to access parts of array than changing it. - Add event handler to the pulldown list to update the plot.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,9 @@
 // Global variables
 const dataFile = 'data/samples.json';
-var metadata, names, samples;
+const selDataset = d3.select('#selDataset');
+const sampleLimit = 10;         // Show top X in the selected sample.
+let metadata, names, samples;
+
 
 // Run once when the page is loaded
 const init = function() {
@@ -14,14 +17,19 @@ const init = function() {
         names = json.names;
         samples = json.samples;
 
+        // Populate the select pulldown list
+
+        names.forEach(name => 
+            selDataset.append('option').text(name).attr('value', name));
+
+
         // Use the first dataset as default to initialize the plot with
         const id = metadata[0].id;
         console.log(`id: ${id}`);   
 
-        const plotData = getPlotDataByID(id, 10);
+        const plotData = getPlotDataByID(id, sampleLimit);
         console.log("Plotdata is: ");
         console.log(plotData);
-
 
         // Add chart type info
         // Add .reserve function to list starting with the largest
@@ -44,6 +52,7 @@ const init = function() {
     });
 };
 
+
 // Returns plot data based on ID
 const getPlotDataByID = function (id, firstX) { 
 
@@ -59,18 +68,37 @@ const getPlotDataByID = function (id, firstX) {
     
     // Limit the top output by first X, if defined
     if (firstX) {
-        plotData.sample_values = plotData.sample_values.splice(0, firstX);
-        plotData.otu_ids =  plotData.otu_ids.splice(0, firstX);
-        plotData.otu_labels =  plotData.otu_labels.splice(0, firstX);
+        plotData.sample_values = plotData.sample_values.slice(0, firstX);
+        plotData.otu_ids =  plotData.otu_ids.slice(0, firstX);
+        plotData.otu_labels =  plotData.otu_labels.slice(0, firstX);
     }
 
     return plotData;
 };
 
-const updatePlot = function(trace) {
 
+const optionChanged = function(id) {
+    // Get the ID number selected
+    //const id = selDataset.node().value;
+    console.log(`selected id ${id}`);
 
+    // Get trace data based on the ID
+    const plotData = getPlotDataByID(id, sampleLimit);
+    console.log("Plotdata is: ");
+    console.log(plotData);
+    const trace = {
+        x: plotData.sample_values.reverse(),
+        y: plotData.otu_ids.map(id => "OTU "+id).reverse(),
+        text: plotData.otu_labels.reverse()
+    };
+
+    // Update the plot
+    Plotly.restyle('bar', 'x', [trace.x]);
+    Plotly.restyle('bar', 'y', [trace.y]);
+    Plotly.restyle('bar', 'text', [trace.text]);
 };
 
 
+// Add event handler for when pull down list is selected
+// selDataset.on('change', updatePlot);
 init();
